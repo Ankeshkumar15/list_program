3815d2b83fed15cfaa306a1390f572ae227619fe
Ankeshkumar15 <142573054+Ankeshkumar15@users.noreply.github.com>
2025-02-27 21:30:39 +0530

    second commit in list

---
+++ list_practices.py
@@ -0,0 +1,22 @@
+#access lists iteams
+boys =["raman", "ankesh", "sintu","abhi"]
+print(boys[1])
+#negative indexing
+girls =["beauty", "shital", "love"]
+print(girls[-1])
+#positive indexing
+a =["ankesh", 43, True, "ram"]
+print(a[2:3])
+
+b =["apple", "banana", "kiwi", "orange"]
+print(b[2:])
+#range of negative indexes
+list =["apple", "banana", "kiwi", "orange"]
+print(list[-2:-4])
+
+#check if item exists
+a =["apple", "banan", "abc"]
+if"apple" in a:
+    print("yes,'apple' is in the fruits list")
+
+
