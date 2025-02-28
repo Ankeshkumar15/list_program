9921b9230dcab7021549ff72bf2cfceb99331bb9
Ankeshkumar15 <142573054+Ankeshkumar15@users.noreply.github.com>
2025-02-28 23:12:24 +0530

    third commit in list

---
+++ list_practices2.py
@@ -0,0 +1,13 @@
+#change list items
+list =["ram", "vivek", "ankesh", "suman"]
+list[1] ="badal"
+print(list)
+
+a = ["apple", "banana", "manago","kiwi"]
+a[1:3] = "watermelon","abcd"
+print(a)
+
+#insert items without removing any items in the list
+a =["apple", "banana","kiwi","aaa"]
+a.insert(3,"abcd")
+print(a)
