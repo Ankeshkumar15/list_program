ac9e20035692db4a2fe3b4c60cb3a5bc2491fc78
Ankeshkumar15 <142573054+Ankeshkumar15@users.noreply.github.com>
2025-03-01 22:22:32 +0530

    fourth commit in list

---
+++ list_practices3.py
@@ -0,0 +1,15 @@
+#append items
+list =["apple", "banana", "kiwi", "orange"]
+list.append("abcd")
+print(list)
+
+#extend items
+list =["apple", "banan", "kiwi", "orange"]
+items =["mobile", "laptop", "tab","tv"]
+list.extend(items)
+print(list)
+
+she =["will you marry me"]
+boy =["no,i am not intersted"]
+she.extend(boy)
+print(she)
